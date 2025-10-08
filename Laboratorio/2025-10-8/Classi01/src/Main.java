ce16278a0b2cd7129cee1ac27dd1b683e0ac4bc4
Leonardo <vigano.leonardo.09@itisdalmine.edu.it>
2025-10-08 13:54:30 +0200

    LL 08/10 classe player

--- Laboratorio/2025-10-8/Classi01/src/Main.java
+++ Laboratorio/2025-10-8/Classi01/src/Main.java
@@ -1,9 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+void main(){
     Mostro m1 = new Mostro();
 
     m1.cambiaNome("Wario");
 
     IO.println("Il mostro è " + m1.mostraNome() + " power: " + m1.mostraEnergia());
+
+    Player p1= new Player();
+    p1.setNome("fefgd");
+    p1.setId("Dragon58");
+    Player p2 = new Player();
+    p2.setNome("fefgd");
+    p2.setId("Dragon5");
+
+    System.out.println("il giocatore 1 si chiama " + p2.getNome());
+
+    //IO.println(p1);
+    // System.out.println(p2);
 }
+
--- Laboratorio/2025-10-8/Classi01/src/Player.java
+++ Laboratorio/2025-10-8/Classi01/src/Player.java
@@ -1,2 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private static List<String> raccoltaID =  new ArrayList<>(); //static crea la stessa lista per ogni oggetto
+    //LA STESSA LISTA PER TUTTI; IN COMUNE
+    //PROVA AD USARE GENERATE
+
+    private String nome;
+    private String nickname;
+    private String id;
+
+    public Player(){
+        nome = "SuperMario";
+    }
+
+    public void setNome(String n){
+        n=n.trim();
+        if (n.length() < 4){
+            throw new IllegalArgumentException("! - Inserisci almento 4 caratteri");
+        }
+        if(n.matches("[a-zA-Z]+")){
+
+        }
+        else throw new IllegalArgumentException("! - Caratteri non ammessi, solo lettere");
+        nome = n;
+    }
+
+    public String getNome (){
+        return nome;
+    }
+
+/*
+    public void setNickname(String n){
+        n=n.trim();
+        if (n.length() < 4){
+            throw new IllegalArgumentException("! - Inserisci almento 4 caratteri");
+        }
+        if(n.matches("[a-zA-Z]+")){
+
+        }
+        else throw new IllegalArgumentException("! - Caratteri non ammessi, solo lettere");
+        nome = n;
+    }
+
+ */
+
+    public void setId(String id){
+        if (!raccoltaID.contains(id)){
+            raccoltaID.add(id);
+        }
+        else throw new IllegalArgumentException("! - ID già usato");
+
+        this.id=id; //this.id è l'attributo, id il parametrp
+    }
+
+    public String getId(){
+        return id;
+    }
+
+
+    @Override //notazione che ti dice "to string esiste gia lo sto sovvrascrivendo"
+    public String toString() { //personalizzare il formao dell'oggetto quando lo stampo
+        return "Player"+"("+id+")";
+    }
 }
+
+
