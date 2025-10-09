a4a51cc829e49ca1bb7a1ec8c88073656769cf67
Leonardo Viganò <vigano.leonardo.09@itisdalmine.edu.it>
2025-10-09 10:00:04 +0200

    Teoria: regole per classi

--- Teoria/classi2/src/Main.java
+++ Teoria/classi2/src/Main.java
@@ -4,7 +4,11 @@ import java.io.InputStreamReader;
 public class Main {
     static BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) {
-        Televisione t1 = new Televisione("Samsung");
+        Televisione t1 = new Televisione("Samsung",5);
+        Televisione t2 = new Televisione(t1); //copia ma diversi non lo stesso!!
+        //ricorda cosa succede in memoria
+
+        t1.setCanale(5);
         //ho creato un nuovo tipo di dato!!
         //cosi imposto gli attributi, t1 e l'oggetto, l'istanza della classe
 
--- Teoria/classi2/src/Televisione.java
+++ Teoria/classi2/src/Televisione.java
@@ -1,13 +1,17 @@
 public class Televisione {
     //attributi funzionali
-    public float dimensione;
-    public String marca;
-    public boolean colore;
+    private int altezza;
+    private int larghezza;
+    private String marca;
 
     //attributi di stato
-    public int volume;
+    private int volume;
     private int canale;
-    public boolean accesa;
+    private boolean accesa;
+
+    //costanti -- final
+    private final int CANALE_MAX=100;
+    private final int VOLUME_MAX=100;
 
     //Information hiding una classe DOVREBBE avere gli attributi private
     //modificatori di accesso!!  se metto private non accedo dall'esterno
@@ -15,9 +19,32 @@ public class Televisione {
 
 
     // costruttoreee nota come non ce la roba void e il nome corrisp alla classe
-    public Televisione(String marca) {
-        marca=marca;
+    public Televisione(String marca, int larghezza, int altezza) {
+        this.marca=marca;
+        canale=1;
+        volume=0;
+        this.altezza=altezza;
+        this.larghezza=larghezza;
+        accesa=false;
         // definisco i valori default
+    }
+
+    public Televisione(String marca, int canale){
+        this.marca=marca;
+        setCanale(canale); //sfrutto e devo sfruttare i controlli
+        volume=0;
+        altezza=0;
+        larghezza=0;
+        accesa=false;
+    }
+    //COSTRUTTORE DI COPIA
+    public Televisione(Televisione b){
+        this.marca=b.marca;
+        this.accesa=b.accesa;
+        this.volume=b.volume;
+        this.altezza=b.altezza;
+        this.larghezza=b.larghezza;
+        this.canale=b.canale;
     }
 
     // metodi
@@ -32,21 +59,39 @@ public class Televisione {
         volume ++;
     }
     public void abbassaVolume(){
-        volume--;
+        if (volume>0) volume--;
+        else throw new IllegalArgumentException("! - Volume minimo raggiunto");
+
     }
     public void incrementaCanale(){
         canale++;
     }
     public void decrementaCanale(){
-        canale--;
+        if (canale>1) canale--;
+        else throw new IllegalArgumentException("! - Tentativo di accesso a canale inesistente");
     }
-    public void impostaCanale (int canale){
+
+//JAVA DOC:
+    /**
+     * Questo metodo serve per impostare il canale
+     * @param canale il canale da impostare non può essere negativo
+     * @throws IllegalArgumentException quando è negativo
+     */
+
+    public void setCanale (int canale){
         if(canale<=0){
-            System.out.println("Il canale non può essere negativo");
+            throw new IllegalArgumentException("! - Canale insesistente");
         }
-        else {
-            this.canale=canale;
-        }
+        this.canale=canale;
+
+    }
+
+    /**
+     *
+     * @return il canale attuale in int
+     */
+    public int getCanale(){
+        return canale;
     }
 
 
