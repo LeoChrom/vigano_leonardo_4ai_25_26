a4a9267ebce9bc6508f7bab7d5c26a58e2838112
Leonardo Viganò <vigano.leonardo.09@itisdalmine.edu.it>
2025-12-11 09:40:10 +0100

    ottimizza finestra login

--- Teoria/Interfacce/FinestraBase/src/Finestra.java
+++ Teoria/Interfacce/FinestraBase/src/Finestra.java
@@ -4,53 +4,41 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
 
-public class Finestra {
+//alleggerisco creo un unico listner
+//devo usare else if perche senno si crea un delay nel bottone
+
+public class Finestra implements ActionListener {
 
     private final int LARGHEZZA_FINESTRA= 400;
     private final int ALTEZZA_FINESTRA= 400;
 
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField usernameTXF;
+    private JTextField passwordTXF;
+    private JButton loginBTN;
+    private JButton clearBTN;
+
     public Finestra(){
         //creo frame
-        JFrame frame = new JFrame("Finestra di login");
+        frame = new JFrame("Finestra di login");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         //componenti
-        JTextField usernameTXF = new JTextField();
+        usernameTXF = new JTextField();
         usernameTXF.setMaximumSize(new Dimension(LARGHEZZA_FINESTRA - 20,20));
-        JTextField passwordTXF = new JTextField();
+        passwordTXF = new JTextField();
         passwordTXF.setMaximumSize(new Dimension(LARGHEZZA_FINESTRA - 20,20));
 
         //creo un bottone
-        JButton loginBTN = new JButton("Esegui login");
-        loginBTN.addActionListener(new ActionListener() {
-            //posso creare al volo un'istanza dell'action listner
-            //Ho una classe anonima attaccata al bottone che non posso recuperare
-            // per programmi complessi la classe anonima non va bene appesantisce
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //recupero dati
-                String username = usernameTXF.getText();
-                String password = passwordTXF.getText();
-                //controllo dati
-                //login
-                if (username.equals("admin") && password.equals("admin")){
-                    JOptionPane.showMessageDialog(null, "login effettuato con successo");
-                }
-                else JOptionPane.showMessageDialog(null, "Errore nel login");
-            }
-        });
+        loginBTN = new JButton("Esegui login");
+        loginBTN.addActionListener(this);
 
-        JButton clearBTN = new JButton("Pulisci form");
-        clearBTN.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                usernameTXF.setText("");
-                passwordTXF.setText("");
-            }
-        });
+        clearBTN = new JButton("Pulisci form");
+        clearBTN.addActionListener(this);
 
         //aggiungi componenti al pannello
         panel.add(usernameTXF);
@@ -67,5 +55,24 @@ public class Finestra {
         frame.setResizable(false);
     }
 
+    //implementazione dell'action listner
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginBTN){
+            //recupero dati
+            String username = usernameTXF.getText();
+            String password = passwordTXF.getText();
+            //controllo dati
+            //login
+            if (username.equals("admin") && password.equals("admin")){
+                JOptionPane.showMessageDialog(null, "login effettuato con successo");
+            }
+            else JOptionPane.showMessageDialog(null, "Errore nel login");
+        }
+        else if (e.getSource() == clearBTN){
+            usernameTXF.setText("");
+            passwordTXF.setText("");
+        }
 
+    }
 }
